bad4d4dcc8ba2286f9cfef6b5c8b801e2f2eeea5
Edward Smith <edwardsmith@Edwards-MacBook-Air.local>
2015-11-12 05:47:37 -0800

    Initial commit

---
+++ scrapper.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+    request = require('request'),
+    cheerio = require('cheerio');
+
+
+url = 'http://www.amazon.com/VIZIO-E50-C1-50-Inch-1080p-Smart/dp/B00SMBFP4U';
+request(url, function(error, response, html) {
+    var price;
+    var json = {
+        price: ""
+    };
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        $('#priceblock_ourprice').each(function(i, element) {
+            var el = $(this);
+            var price = el.text();
+            console.log(price);
+            json.price = price;
+
+            fs.writeFile('price.json', JSON.stringify(json, null, 4), function(err) {
+                console.log("Price saved to json");
+            });
+            fs.readFile('price.json', function(err, data) {
+                if (err) throw err;
+                var obj = JSON.parse(data);
+                if (obj.price != price) {
+                    console.log('Price has changed.');
+                    fs.writeFile('price.json', JSON.stringify(json, null, 4), function(err) {
+                        console.log('Price updated in price.json file');
+                    });
+                }
+            })
+        })
+    }
+});
