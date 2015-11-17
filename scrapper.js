2f0399b7b02387f547d3d6211a8c058c8aaee088
Chops <chops@goraver.com>
2015-11-17 01:06:38 -0800

    Added email features

--- scrapper.js
+++ scrapper.js
@@ -6,31 +6,36 @@ nodemailer = require('nodemailer')
 var smtpTransport = nodemailer.createTransport("SMTP", {
     service: "Gmail",
     auth: {
-        user: "grnfusion@gmail.com",
-        pass: "g1pp3r10"
+        user: "",
+        pass: ""
     }
 });
 
 url = 'http://www.amazon.com/VIZIO-E50-C1-50-Inch-1080p-Smart/dp/B00SMBFP4U';
 request(url, function(error, response, html) {
-    var price;
+    var price, oldprice;
     var json = {
-        price: ""
+        price: "",
+        oldprice: ""
     };
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
+        var productTitle = $('#productTitle').text();
         $('#priceblock_ourprice').each(function(i, element) {
             var el = $(this);
             var price = el.text();
-            console.log(price);
+            //console.log(price);
             json.price = price;
+            json.oldprice = price;
+            console.log(json)
+            
 
 
 
             fs.readFile('price.json', function(err, data) {
                 if (err) throw err;
                 var obj = JSON.parse(data);
-                if (obj.price != price) {
+                if (obj.price != obj.oldprice) {
                     console.log('Price has changed.');
                     fs.writeFile('price.json', JSON.stringify(json, null, 4), function(err) {
                         console.log('Price saved in price.json file');
@@ -38,8 +43,8 @@ request(url, function(error, response, html) {
                     smtpTransport.sendMail({
                         from: "Amz BOt <amazonbot@amzbots.com>", // sender address
                         to: "Ed Smith  <deltonamazon@mailinator.com>", // comma separated list of receivers
-                        subject: "Hello", // Subject line
-                        text: "Price for has changed" // plaintext body
+                        subject: productTitle + " price change", // Subject line
+                        text: "Price  has changed to " + json.price + " from " + obj.oldprice // plaintext body
                     }, function(error, response) {
                         if (error) {
                             console.log(error);
